6a78b064e6702e8b650bcb548aff0378b8c53c43
EduardsZv <eduagg123@gmail.com>
2024-06-05 16:34:09 +0300

    Updated "Create task" component, v1.1

--- taskManagerApp/src/app/create-task/create-task.component.ts
+++ taskManagerApp/src/app/create-task/create-task.component.ts
@@ -1,3 +1,4 @@
+import { DatePipe } from '@angular/common';
 import { Component } from '@angular/core';
 import {FormControl, FormGroup} from '@angular/forms';
 import { ReactiveFormsModule} from '@angular/forms';
@@ -7,6 +8,7 @@ import { ReactiveFormsModule} from '@angular/forms';
   selector: 'tmg-create-task',
   standalone: true,
   imports: [ReactiveFormsModule],
+  providers: [DatePipe],
   templateUrl: './create-task.component.html',
   styleUrl: './create-task.component.scss'
 })
@@ -19,7 +21,13 @@ export class CreateTaskComponent {
     status: new FormControl("")
   })
 
+  constructor(private datePipe: DatePipe) {}
+
   submitTask() {
+    let currTime = new Date();
+    let submit_time = this.datePipe.transform(currTime, 'yyyy-MM-dd HH:mm:ss');
+    this.createTaskForm.get('createdOn')?.setValue(new Date().toString());
     console.log(this.createTaskForm.value);
   }
+
 }
