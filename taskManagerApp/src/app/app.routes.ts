6011b6802a59f320806d5661c663609b340d9f63
EduardsZv <eduagg123@gmail.com>
2024-06-06 20:00:31 +0300

    Created navbar, implemented routing for "Create task" component

--- taskManagerApp/src/app/app.routes.ts
+++ taskManagerApp/src/app/app.routes.ts
@@ -1,3 +1,10 @@
 import { Routes } from '@angular/router';
+import { CreateTaskComponent } from './create-task/create-task.component';
+import { DisplayTasksComponent } from './display-tasks/display-tasks.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {
+        path: 'create-task-component',
+        component: CreateTaskComponent
+    }
+];
